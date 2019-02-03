befc0fddfc224d54dfd2a50f82ee9093d3e520f7
Jay Prakash Mishra <47255342+jayprakashmishra@users.noreply.github.com>
2019-02-03 22:47:38 +0530

    Selection sort using python
    selection sort

---
+++ Patern_print_with_ABCD.py
@@ -0,0 +1,12 @@
+
+for i in range(1,10):
+    for j in range(1,i):
+        print(j,end=" ")  
+    print("\n")
+for k in range(0,10):
+    for l in range(k,9):
+        print("*",end=" ")
+    print("\n")
+
+
+
