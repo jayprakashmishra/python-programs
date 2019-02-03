69eae0a518d3915dca000c6a90efd395df553308
Jay Prakash Mishra <47255342+jayprakashmishra@users.noreply.github.com>
2019-02-03 22:24:17 +0530

    simple Half x print using python
    *
     * *
       * * *
     * *
    *
    Print  using python programming

---
+++ Half_X_print.py
@@ -0,0 +1,15 @@
+for i in range(0,10):
+    for j in range(0,i+1):
+        print("*",end=" ")
+        
+    print("\n")    
+    for k in range(0,j):
+        print(" ", end=" ")   
+       
+for k in range(0,10):
+    for l in range(k,10):
+        print("*",end=" ")
+    print("\n")
+    for n in range(k,8):
+        print(" ",end=" ")
+    
